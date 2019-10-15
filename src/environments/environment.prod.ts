5998a6dddefac5ca9fe84a09f55b493f6edd055b
Paul <validateus@yahoo.com>
2019-10-15 14:20:13 +0700

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAMggc8aFqy6XDj8Ry2ZPt4sEEDLEQaR94'
 };
